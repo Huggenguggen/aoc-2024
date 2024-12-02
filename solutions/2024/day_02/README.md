6373a5f75661b3f8b8dd9d79bbbb1e62a1053bf3
Yu Heng Lee <lee2000yh@gmail.com>
2024-12-02 14:38:09 +0800

    Day 2 done

--- solutions/2024/day_02/README.md
+++ solutions/2024/day_02/README.md
@@ -1,10 +1,42 @@
 # Day 2 (2024)
 
-`TITLE` ([prompt](https://adventofcode.com/2024/day/2))
+`Red-Nosed Reports` ([prompt](https://adventofcode.com/2024/day/2))
 
-Use this space for notes on the day's solution and to document what you've learned!
+The unusual data (your puzzle input) consists of many reports, one report per line. Each report is a list of numbers called levels that are separated by spaces. For example:
+
+```
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+```
+
+This example data contains six reports each containing five levels.
+
+The engineers are trying to figure out which reports are safe. The Red-Nosed reactor safety systems can only tolerate levels that are either gradually increasing or gradually decreasing. So, a report only counts as safe if both of the following are true:
+
+The levels are either **all increasing** or **all decreasing**.
+Any two adjacent levels differ by **at least one** and **at most three**.
+
 
 ## Part 1
+Analyze the unusual data from the engineers. How many reports are safe?
+### Approach
+either monotomically increasing or decreasing to be safe  
+also needs to be differing by a certain amount
+
+- for each line:
+  - first check if it's increasing or decreasing line[0] < line[1], if equal short-circuit 
+  - flag it isIncreasing bool
+  - then go through the list and check if it matches the criteria
+
 
 ## Part 2
+The Problem Dampener is a reactor-mounted module that lets the reactor safety systems tolerate a single bad level in what would otherwise be a safe report. It's like the bad level never happened!
 
+Now, the same rules apply as before, except if removing a single level from an unsafe report would make it safe, the report instead counts as safe.
+
+### Approach
+Same as above but just try every combi of removing one element from the list to see if there is a valid one
--- solutions/2024/day_02/solution.py
+++ solutions/2024/day_02/solution.py
@@ -3,19 +3,64 @@
 # puzzle prompt: https://adventofcode.com/2024/day/2
 
 from ...base import StrSplitSolution, answer
-
+from typing import List
 
 class Solution(StrSplitSolution):
     _year = 2024
     _day = 2
 
-    # @answer(1234)
+    @answer(279)
     def part_1(self) -> int:
-        pass
+        safe = 0
+        for line in self.input:
+            line = [int(x) for x in line.split(" ")]
+            # first check if inc or dec
+            isIncreasing = None
+            if line[0] < line[1]:
+                isIncreasing = True 
+            elif line[1] < line[0]:
+                isIncreasing = False 
+            else:
+                # unsafe
+                continue
+            if self.part1Safe(line, isIncreasing):
+                safe += 1
+        return safe 
 
-    # @answer(1234)
+    def part1Safe(self, line: List[int], isIncrease: bool) -> bool:
+        if isIncrease:
+            for i in range(len(line) - 1):
+                if line[i] >= line[i + 1] or \
+                    line[i] < (line[i + 1] - 3):
+                    return False
+        
+            return True 
+        else: # decreasing
+            for i in range(len(line) - 1):
+                if line[i] <= line[i + 1] or \
+                    line[i + 1] < (line[i] - 3):
+                    return False
+        
+            return True
+
+    @answer(343)
     def part_2(self) -> int:
-        pass
+        safe = 0
+        for line in self.input:
+            line = [int(x) for x in line.split(" ")]
+            if self.part2Safe(line) or \
+                any(self.part2Safe(line[:i] + line[i + 1:]) \
+                    for i in range(len(line))):
+                safe += 1
+        return safe 
+
+    def part2Safe(self, line: List[int]) -> bool:
+        diffs = [abs(curr - prev) for prev, curr in zip(line[:-1], line[1:])]
+        isInc = all(i < j for i,j in zip(line[:-1], line[1:]))
+        isDec = all(i > j for i,j in zip(line[:-1], line[1:]))
+
+        isDiff = all(d in (1,2,3) for d in diffs)
+        return (isDec or isInc) and isDiff
 
     # @answer((1234, 4567))
     # def solve(self) -> tuple[int, int]:
