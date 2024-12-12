cd1349f345b59c56db0b89c3f3695763e27658ad
Yu Heng Lee <89116915+Huggenguggen@users.noreply.github.com>
2024-12-12 16:06:47 +0800

    Day 12 complete

---
+++ solutions/2024/day_12/README.md
@@ -0,0 +1,38 @@
+# Day 12 (2024)
+
+`Garden Groups` ([prompt](https://adventofcode.com/2024/day/12))
+
+They'd like to set up fences around each region of garden plots, but they can't figure out how much fence they need to order or how much it will cost. They hand you a map (your puzzle input) of the garden plots.
+
+Each garden plot grows only a single type of plant and is indicated by a single letter on your map. When multiple garden plots are growing the same type of plant and are touching (horizontally or vertically), they form a region. For example:
+```
+AAAA
+BBCD
+BBCC
+EEEC
+```
+This 4x4 arrangement includes garden plots growing five different types of plants (labeled A, B, C, D, and E), each grouped into their own region.
+
+In order to accurately calculate the cost of the fence around a single region, you need to know that region's area and perimeter.
+
+The area of a region is simply the number of garden plots the region contains. The above map's type A, B, and C plants are each in a region of area 4. The type E plants are in a region of area 3; the type D plants are in a region of area 1.
+
+Each garden plot is a square and so has four sides. The perimeter of a region is the number of sides of garden plots in the region that do not touch another garden plot in the same region. The type A and C plants are each in a region with perimeter 10. The type B and E plants are each in a region with perimeter 8. The lone D plot forms its own region with perimeter 4.
+
+Due to "modern" business practices, the price of fence required for a region is found by multiplying that region's area by its perimeter. The total price of fencing all regions on a map is found by adding together the price of fence for every region on the map.
+
+
+## Part 1
+What is the total price of fencing all regions on your map?
+
+### Approach
+Can bfs to find regions, get area and perimeter
+- note that perimeter is **NOT** solely based off the number of plants (i.e. area)
+  - 4 can be 8 or 10
+  - idea: for each direction it is connected to an already seen square, you -2 
+
+
+## Part 2
+Fortunately, the Elves are trying to order so much fence that they qualify for a bulk discount!
+
+Under the bulk discount, instead of using the perimeter to calculate the price, you need to use the number of sides each region has. Each straight section of fence counts as a side, regardless of how long it is.
---
+++ solutions/2024/day_12/solution.py
@@ -0,0 +1,136 @@
+# Generated using @xavdid's AoC Python Template: https://github.com/xavdid/advent-of-code-python-template
+
+# puzzle prompt: https://adventofcode.com/2024/day/12
+
+from ...base import CharSplitSolution, answer
+from collections import deque
+from typing import Tuple
+
+class Solution(CharSplitSolution):
+    _year = 2024
+    _day = 12
+
+    # @answer(1234)
+    def part_1(self) -> int:
+        self.seen = set()
+        total = 0
+        for i in range(len(self.input)):
+            for j in range(len(self.input[0])):
+                if (i, j) in self.seen:
+                    continue 
+                area, perimeter = self.bfs(i, j, self.input[i][j])
+                # print(f"Crop: {self.input[i][j]}, Area: {area}, Perimeter: {perimeter}")
+                total += area * perimeter
+        return total
+
+    def bfs(self, i: int, j: int, crop: str) -> Tuple[int, int]:
+        visited = set()
+        area = 0
+        perimeter = 0
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        q = deque([(i, j)])
+        while q:
+            for _ in range(len(q)):
+                currX, currY = q.popleft()
+                if self.input[currX][currY] == crop and not (currX, currY) in visited:
+                    visited.add((currX, currY))
+                    area += 1
+                    perimeter += 4
+                    for dx, dy in directions:
+                        if (currX + dx, currY + dy) in visited:
+                            perimeter -= 2
+                        else:
+                            if (currX + dx) in range(0, len(self.input))\
+                                  and (currY + dy) in range(0, len(self.input[0])):
+                                q.append((currX + dx, currY + dy))
+                else:
+                    continue
+        self.seen |= visited
+        return (area, perimeter)
+
+    # @answer(1234)
+    def part_2(self) -> int:
+        visited = set()
+        total = 0
+        for i in range(len(self.input)):
+            for j in range(len(self.input[0])):
+                if (i, j) in visited:
+                    continue 
+                area, perimeter, corner = self.bfs2(i, j, self.input[i][j], visited)
+                # print(f"Crop: {self.input[i][j]}, Area: {area}, Perimeter: {perimeter}")
+                total += area * corner
+        return total        
+
+
+    def grid_val(self, i, j):
+        if i in range(0, len(self.input)) and j in range(0, len(self.input[0])):
+            return self.input[i][j]
+        else:
+            return ' '
+
+    def bfs2(self, i: int, j: int, crop: str, visited: set) -> Tuple[int, int, int]:
+        area = 1
+        perim = 0
+        corners = 0
+        visited.add((i,j))
+
+        # get neighbors
+        ch_up = self.grid_val(i-1, j)
+        ch_down = self.grid_val(i+1, j)
+        ch_left = self.grid_val(i, j-1)
+        ch_right = self.grid_val(i, j+1)
+
+        # perimeter
+        if ch_up != crop:
+            perim += 1
+        if ch_down != crop:
+            perim += 1
+        if ch_left != crop:
+            perim += 1
+        if ch_right != crop:
+            perim += 1
+
+        # corners
+        if ch_up != crop and ch_right != crop:
+            corners += 1
+        if ch_right != crop and ch_down != crop:
+            corners += 1
+        if ch_down != crop and ch_left != crop:
+            corners += 1
+        if ch_left != crop and ch_up != crop:
+            corners += 1
+
+        # 'negative' corners
+        if self.grid_val(i-1, j-1) != crop and ch_up == crop and ch_left == crop:
+            corners += 1
+        if self.grid_val(i-1, j+1) != crop and ch_up == crop and ch_right == crop:
+            corners += 1
+        if self.grid_val(i+1, j+1) != crop and ch_down == crop and ch_right == crop:
+            corners += 1
+        if self.grid_val(i+1, j-1) != crop and ch_down == crop and ch_left == crop:
+            corners += 1
+            
+        if ch_up == crop and not (i-1,j) in visited:
+            a, p, c = self.bfs2(i-1,j,crop,visited)
+            area += a
+            perim += p
+            corners += c
+        if ch_down == crop and not (i+1,j) in visited:
+            a, p, c = self.bfs2(i+1,j,crop,visited)
+            area += a
+            perim += p
+            corners += c
+        if ch_left == crop and not (i,j-1) in visited:
+            a, p, c = self.bfs2(i,j-1,crop,visited)
+            area += a
+            perim += p
+            corners += c
+        if ch_right == crop and not (i,j+1) in visited:
+            a, p, c = self.bfs2(i,j+1,crop,visited)
+            area += a
+            perim += p
+            corners += c
+        return([area, perim, corners])
+    # @answer((1234, 4567))
+    # def solve(self) -> tuple[int, int]:
+    #     pass
